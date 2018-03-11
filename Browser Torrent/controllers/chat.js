b02a645d0435f782a3cd91f154edb6fe0bb61c7b
dhirajs1997 <32067769+dhirajs1997@users.noreply.github.com>
2018-03-11 23:39:28 +0530

    Message on finishing download

--- Browser Torrent/controllers/chat.js
+++ Browser Torrent/controllers/chat.js
@@ -8,6 +8,9 @@ var downspeed = document.getElementById('downspeed');
 
 //Listen for stats update
 socket.on('stats',function(data){
+  if(data.progress == 100){
+    document.getElementById('status').innerHTML = 'Download Finished!'
+  }
   progress.innerHTML = '<strong>' + data.progress + '</strong>' + '%';
   downspeed.innerHTML = '<strong>' + data.downloadSpeed + '</strong>' + ' KBps';
 });
